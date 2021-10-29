f40b42c89bca6ff14b42fe6c0b8acaba27895991
leandrofreire13 <rafaelleandrofreire@gmail.com>
2021-10-28 23:25:43 -0300

    Criando o forms para o front e cadastrar referidos

--- referidos/views.py
+++ referidos/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 
 from .models import Referido
+from .forms import ReferidoForm
 
 
 # Create your views here.
@@ -13,3 +14,9 @@ def index(request):
 def list_referidos(request):
     referidos = Referido.objects.filter()
     return render(request, 'referidos/lista_referidos.html', {'referidos': referidos})
+
+
+# Cria um novo referido
+
+def referido_new(request):
+    return HttpResponse("Arrumar esse def para criar o forms")
