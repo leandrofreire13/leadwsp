4c4f34ac47d998b05c81ceb2c9d7ee9bd470e744
leandrofreire13 <rafaelleandrofreire@gmail.com>
2021-10-30 20:45:59 -0300

    Rodando no heroku

--- leadwsp/settings.py
+++ leadwsp/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-#import django_heroku
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,4 +124,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
