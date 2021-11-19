d9d458e8bbe1c7c6987f9c19f5da2f08917f899c
leandrofreire13 <rafaelleandrofreire@gmail.com>
2021-11-19 15:55:43 -0300

    Tryng

--- leadwsp/settings.py
+++ leadwsp/settings.py
@@ -38,9 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'referidos.apps.ReferidosConfig',
-    'django_bootstrap_icons'
-]
+    'referidos.apps.ReferidosConfig']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
