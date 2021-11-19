2e20aacb08add72d3c70df2f570e3f5d129ccbc4
leandrofreire13 <rafaelleandrofreire@gmail.com>
2021-11-19 16:19:56 -0300

    Estilo css

--- leadwsp/settings.py
+++ leadwsp/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'leadwsp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join((BASE_DIR), 'templates')],
+        'DIRS': [os.path.join((BASE_DIR), 'templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,7 +125,8 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/'
 
