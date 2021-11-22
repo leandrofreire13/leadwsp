2a86dd119803a01e22fb1ecdee9c5ab893a3e225
leandrofreire13 <rafaelleandrofreire@gmail.com>
2021-11-22 15:48:45 -0300

    Class for bootstrap

--- referidos/forms.py
+++ referidos/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from django.forms import TextInput
 
 from .models import Referido
 
@@ -7,4 +7,16 @@ from .models import Referido
 class ReferidoForm(forms.ModelForm):
     class Meta:
         model = Referido
-        fields = {'quem_indicou', 'nome', 'telefone', 'prioridade', 'status'}
+        fields = ['quem_indicou', 'nome', 'telefone', 'prioridade', 'status']
+        widgets = {
+            'quem_indicou': TextInput(attrs={
+                'class': "form-control"
+            }),
+            'nome': TextInput(attrs={
+                'class': "form-control"
+            }),
+            'telefone': TextInput(attrs={
+                'class': "form-control"
+            }),
+
+        }
