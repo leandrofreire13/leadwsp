aac3be9bc74cd84aa5348d5c89b583bc79a578fc
leandrofreire13 <rafaelleandrofreire@gmail.com>
2021-11-26 14:53:55 -0300

    Listando os referidos

--- referidos/urls.py
+++ referidos/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('referidos', views.list_referidos, name='referidos'),
+    path('referidos/dados', views.lista_dados, name='dados'),
     path('referidos/new', views.referido_new, name="referido_new"),
     path('referidos/<int:pk>/edit', views.referido_edit, name="referido_edit"),
 ]
